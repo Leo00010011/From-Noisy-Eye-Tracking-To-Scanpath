5fc4a74a409ac33df43a85ef88de0202eb31a931
lulloa <lulloa@datamart.co>
2025-10-29 20:50:29 +0100

    New timeline segmentation  tested

---
+++ test.py
@@ -0,0 +1,12 @@
+from src.datasets import CocoFreeView
+from src.simulation import gen_gaze
+
+data = CocoFreeView()
+print('s')
+gaze, fixations, fixation_mask = gen_gaze(data,
+                                          5164, 
+                                          60,
+                                          get_scanpath=True,
+                                          get_fixation_mask=True)
+print('e')
+
