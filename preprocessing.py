35b1f995bbca4217686722aeb7e1c6bf94967e45
lulloa <lulloa@datamart.co>
2025-11-03 18:40:16 +0100

    In Memory Working

---
+++ preprocessing.py
@@ -0,0 +1,60 @@
+    
+import h5py
+from src.preprocess.simulation import gen_gaze, downsample
+from src.datasets import CocoFreeView
+
+
+def preprocess(sampling_rate, downsample_int,min_scanpath_duration,sample_size,max_fixation_duration, log):
+    data = CocoFreeView()
+    gen_data = []
+    original_data_count = len(data)
+    for index in range(original_data_count):
+        gaze, fixations, fixation_mask = gen_gaze(data,
+                                                    index, sampling_rate,
+                                                    get_scanpath=True,
+                                                    get_fixation_mask=True)
+        down_gaze = downsample(gaze, down_time_step=downsample_int)
+        if (gaze[2,-1] < max(min_scanpath_duration, (sample_size - 1)*downsample_int) or
+            fixations[2].max() > max_fixation_duration) :
+            continue
+        gen_data.append({'down_gaze': down_gaze,
+                            'fixations': fixations,
+                            'fixation_mask': fixation_mask,
+                            'gaze': gaze})
+    if log:
+        removed = original_data_count - len(gen_data)
+        print(f'Removed: {removed} - {(removed/original_data_count)*100}% ')
+    # save
+    return gen_data
+
+def save_gen_data(save_path, gen_data, log):
+    with h5py.File(save_path, 'w') as f:
+        # Create datasets with shape (43000,) and the vlen dtype
+        item = gen_data[0]
+        k_dset = dict()
+        for k in item.keys():
+            k_dset[k] = f.create_dataset(k, len(gen_data), dtype= h5py.special_dtype(vlen= item[k].dtype))
+
+
+        # Loop and store each item
+        for i, item in enumerate(gen_data):
+            for k in item.keys():
+                if item[k].ndim > 1:
+                    k_dset[k][i] = item[k].flatten()
+                else:
+                    k_dset[k][i] = item[k]
+    
+    if log:
+        print('generated items saved')
+
+
+if __name__ == "__main__":
+    sampling_rate = 60
+    downsample_int = 200
+    min_scanpath_duration = 3000
+    sample_size = 128
+    max_fixation_duration = 1200
+    save_path = 'data/Coco_FreeView_preprocessed.h5'
+    log = True
+    gen_data = preprocess(sampling_rate, downsample_int, min_scanpath_duration, sample_size, max_fixation_duration, log)
+    save_gen_data(save_path, gen_data, log=log)
