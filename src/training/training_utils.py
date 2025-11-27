583ab9292e89afa6bd1f05abc2f93b8d369857e4
lulloa <lulloa@datamart.co>
2025-11-27 16:36:50 +0100

    addign regression and duration error to metric storage

--- src/training/training_utils.py
+++ src/training/training_utils.py
@@ -3,6 +3,57 @@ import numpy as np
 import json
 from src.eval.eval_metrics import create_cls_targets, accuracy, precision, recall, eval_reg
 
+class MetricsStorage:
+    def __init__(self, filepath: str = None, decisive_metric: str = 'reg_loss_val'):
+        self.metrics = {
+            'epoch': [],
+            'reg_loss_train': [],
+            'reg_error_val': [],
+            'duration_error_val': [],
+            'reg_loss_val': [],
+            'cls_loss_train': [],
+            'cls_loss_val': [],
+            'accuracy': [],
+            'precision_pos': [],
+            'recall_pos': [],
+            'precision_neg': [],
+            'recall_neg': []
+        }
+        self.total_reg_loss = 0
+        self.total_cls_loss = 0
+        self.num_batches = 0
+        self.filepath = filepath
+        self.decisive_metric = decisive_metric
+        self.best_metric_value = np.inf
+    
+    def init_epoch(self):
+        self.total_reg_loss = 0
+        self.total_cls_loss = 0
+        self.num_batches = 0
+
+    def update_batch_loss(self, reg_loss, cls_loss):
+        self.total_reg_loss += reg_loss.item()
+        self.total_cls_loss += cls_loss.item()
+        self.num_batches += 1
+    
+    def finalize_epoch(self):
+        avg_reg_loss = self.total_reg_loss / self.num_batches
+        avg_cls_loss = self.total_cls_loss / self.num_batches
+        self.metrics['reg_loss_train'].append(avg_reg_loss)
+        self.metrics['cls_loss_train'].append(avg_cls_loss)
+        return avg_reg_loss, avg_cls_loss
+    
+    def update_best(self):
+        if self.metrics[self.decisive_metric][-1] < self.best_metric_value:
+            self.best_metric_value = self.metrics[self.decisive_metric][-1]
+            return True
+        return False
+
+    def save_metrics(self):
+        with open(self.filepath, 'w') as f:
+            json.dump(self.metrics, f)
+
+
 def validate(model, val_dataloader, epoch, device, metrics, log = True):
     model.eval()
     with torch.no_grad():
@@ -13,8 +64,8 @@ def validate(model, val_dataloader, epoch, device, metrics, log = True):
         rec_neg_acum = 0
         reg_loss_acum = 0
         cls_loss_acum = 0
-        reg_acc_acum = 0
-        dur_acum = 0
+        reg_error_acum = 0
+        duration_error_acum = 0
         cnt = 0
         for batch in val_dataloader:
             input = move_data_to_device(batch, device)
@@ -35,12 +86,12 @@ def validate(model, val_dataloader, epoch, device, metrics, log = True):
             pre_neg_acum += precision(cls_out, y_mask, cls_targets, cls = 0)
             rec_neg_acum += recall(cls_out, y_mask, cls_targets, cls = 0)
             reg_error, duration_error = eval_reg(reg_out, y, y_mask)
-            reg_acc_acum += reg_error
-            dur_acum += duration_error
+            reg_error_acum += reg_error
+            duration_error_acum += duration_error
             cnt += 1
         metrics['epoch'].append(epoch + 1)
-        metrics['reg_error_val'].append(reg_acc_acum / cnt)
-        metrics['duration_error_val'].append(dur_acum / cnt)
+        metrics['reg_error_val'].append(reg_error_acum / cnt)
+        metrics['duration_error_val'].append(duration_error_acum / cnt)
         metrics['reg_loss_val'].append(reg_loss_acum / cnt)
         metrics['cls_loss_val'].append(cls_loss_acum / cnt)
         metrics['accuracy'].append(acc_acum / cnt)
@@ -52,7 +103,9 @@ def validate(model, val_dataloader, epoch, device, metrics, log = True):
         if log:
             print(f'>>>>>>> Validation results at epoch {metrics["epoch"][-1]}:')
             print('reg_loss_val: ',metrics['reg_loss_val'][-1])
-            print('cls_loss_val: ',metrics['cls_loss_val'][-1])            
+            print('cls_loss_val: ',metrics['cls_loss_val'][-1])  
+            print('reg_error_val: ',metrics['reg_error_val'][-1])
+            print('duration_error_val: ',metrics['duration_error_val'][-1])
             print('accuracy: ',metrics['accuracy'][-1])
             print('precision_pos: ',metrics['precision_pos'][-1])
             print('recall_pos: ',metrics['recall_pos'][-1])
@@ -106,51 +159,3 @@ def move_data_to_device(batch, device):
             device_batch[key] = item.to(device=device)
     return device_batch
 
-
-class MetricsStorage:
-    def __init__(self, filepath: str = None, decisive_metric: str = 'reg_loss_val'):
-        self.metrics = {
-            'epoch': [],
-            'reg_loss_train': [],
-            'reg_loss_val': [],
-            'cls_loss_train': [],
-            'cls_loss_val': [],
-            'accuracy': [],
-            'precision_pos': [],
-            'recall_pos': [],
-            'precision_neg': [],
-            'recall_neg': []
-        }
-        self.total_reg_loss = 0
-        self.total_cls_loss = 0
-        self.num_batches = 0
-        self.filepath = filepath
-        self.decisive_metric = decisive_metric
-        self.best_metric_value = np.inf
-    
-    def init_epoch(self):
-        self.total_reg_loss = 0
-        self.total_cls_loss = 0
-        self.num_batches = 0
-
-    def update_batch_loss(self, reg_loss, cls_loss):
-        self.total_reg_loss += reg_loss.item()
-        self.total_cls_loss += cls_loss.item()
-        self.num_batches += 1
-    
-    def finalize_epoch(self):
-        avg_reg_loss = self.total_reg_loss / self.num_batches
-        avg_cls_loss = self.total_cls_loss / self.num_batches
-        self.metrics['reg_loss_train'].append(avg_reg_loss)
-        self.metrics['cls_loss_train'].append(avg_cls_loss)
-        return avg_reg_loss, avg_cls_loss
-    
-    def update_best(self):
-        if self.metrics[self.decisive_metric][-1] < self.best_metric_value:
-            self.best_metric_value = self.metrics[self.decisive_metric][-1]
-            return True
-        return False
-
-    def save_metrics(self):
-        with open(self.filepath, 'w') as f:
-            json.dump(self.metrics, f)
