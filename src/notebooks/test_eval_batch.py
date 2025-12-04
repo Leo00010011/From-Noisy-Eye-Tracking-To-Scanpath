735557cf76bb94cc400d3fc2e9f696daae00b23d
lulloa <lulloa@datamart.co>
2025-12-04 18:35:41 +0100

    adding line for saving outputs

--- src/notebooks/test_eval_batch.py
+++ src/notebooks/test_eval_batch.py
@@ -128,9 +128,9 @@ for i, (model, _, _, test_dataloader) in enumerate(models_and_data):
         with torch.no_grad():
             output = model(**input)
             inputs, outputs = invert_transforms(input, output, test_dataloader)
+            inputs_outputs.append((inputs, outputs))
             cls_loss, reg_loss = compute_loss(inputs, outputs)
             print(f'Cls Loss: {cls_loss:.4f}, Reg Loss: {reg_loss:.4f}')
-            inputs_outputs.append((inputs, outputs))
             reg_out, cls_out = outputs['reg'], outputs['cls']
             y, y_mask, fixation_len = inputs['tgt'], inputs['tgt_mask'], inputs['fixation_len']
             cls_targets = create_cls_targets(cls_out, fixation_len)
@@ -146,3 +146,4 @@ for i, (model, _, _, test_dataloader) in enumerate(models_and_data):
     torch.cuda.empty_cache()
     gc.collect()
     
+torch.save(inputs_outputs, 'inputs_outputs.pth')
