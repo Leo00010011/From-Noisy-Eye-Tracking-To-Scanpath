1da74063781dddc0d9c60593cdb8d2dec91055b6
lulloa <lulloa@datamart.co>
2025-12-09 13:08:18 +0100

    adding precision

--- best_models.py
+++ best_models.py
@@ -19,15 +19,23 @@ for output_path in out.rglob('*.json'):
         continue
     idx = list(range(len(metric['reg_error_val'])))
     min_idx = min(idx, key = lambda x: metric['reg_error_val'][x])
-    model_best_coord_error.append( (metric['reg_error_val'][min_idx], metric['duration_error_val'][min_idx], metric['recall_pos'][min_idx]))
-    model_best_recall_pos.append( max(metric['recall_pos']))
+    model_best_coord_error.append( (metric['reg_error_val'][min_idx], 
+                                    metric['duration_error_val'][min_idx],
+                                    metric['recall_pos'][min_idx],
+                                    metric['precision_pos'][min_idx]))
+    min_idx = min(idx, key = lambda x: metric['duration_error_val'][x])
+    model_best_dur_error.append( (metric['reg_error_val'][min_idx], 
+                                    metric['duration_error_val'][min_idx],
+                                    metric['recall_pos'][min_idx],
+                                    metric['precision_pos'][min_idx]))
+    model_best_recall_pos.append( min(metric['duration_error_val']))
 idx = list(range(len(model_best_coord_error)))
 print('coord error')
 idx.sort(key = lambda x: model_best_coord_error[x])
 for i in idx[:4]:
     print(str(path_list[i]), f' {model_best_coord_error[i]}')
-print('recall pos')
-idx.sort(key = lambda x: model_best_recall_pos[x], reverse=True)
+print('duration pos')
+idx.sort(key = lambda x: model_best_recall_pos[x])
 for i in idx[:4]:
-    print(str(path_list[i]), f' {model_best_recall_pos[i]}')
+    print(str(path_list[i]), f' {model_best_dur_error[i]}')
     
