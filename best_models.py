78194a1997f05c7830851d555e6e9516d1e37c7c
lulloa <lulloa@datamart.co>
2025-12-09 13:22:09 +0100

    adding good recall

--- best_models.py
+++ best_models.py
@@ -5,7 +5,7 @@ out = Path('outputs')
 path_list = []
 model_best_coord_error = []
 model_best_dur_error = []
-model_best_recall_pos = []
+model_best_dur_with_good_recall = []
 for output_path in out.rglob('*.json'):
     try:
         with open(output_path, 'r') as f:
@@ -15,7 +15,7 @@ for output_path in out.rglob('*.json'):
     if 'reg_error_val' not in metric:
         continue
     path_list.append(output_path)
-    if min(metric['reg_error_val']) < 10 or max(metric['recall_pos']) < 0.03:
+    if min(metric['reg_error_val']) < 10:
         continue
     idx = list(range(len(metric['reg_error_val'])))
     min_idx = min(idx, key = lambda x: metric['reg_error_val'][x])
@@ -28,14 +28,25 @@ for output_path in out.rglob('*.json'):
                                     metric['duration_error_val'][min_idx],
                                     metric['recall_pos'][min_idx],
                                     metric['precision_pos'][min_idx]))
-    model_best_recall_pos.append( min(metric['duration_error_val']))
+    if metric['recall_pos'][min_idx] > 0.30:
+        model_best_dur_with_good_recall.append( (metric['reg_error_val'][min_idx], 
+                                        metric['duration_error_val'][min_idx],
+                                        metric['recall_pos'][min_idx],
+                                        metric['precision_pos'][min_idx]))
+print(model_best_coord_error)
 idx = list(range(len(model_best_coord_error)))
 print('coord error')
 idx.sort(key = lambda x: model_best_coord_error[x])
 for i in idx[:4]:
     print(str(path_list[i]), f' {model_best_coord_error[i]}')
 print('duration pos')
-idx.sort(key = lambda x: model_best_recall_pos[x])
+idx.sort(key = lambda x: model_best_dur_error[x])
 for i in idx[:4]:
     print(str(path_list[i]), f' {model_best_dur_error[i]}')
-    
+if model_best_dur_with_good_recall:
+    print('duration with good recall')
+    idx.sort(key = lambda x: model_best_dur_with_good_recall[x])
+    for i in idx[:4]:
+        print(str(path_list[i]), f' {model_best_dur_with_good_recall[i]}')
+else:
+    print('no model with good recall')
