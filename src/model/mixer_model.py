fb84797cf927b46ba660de9b4a3caed6b6939a8c
lulloa <lulloa@datamart.co>
2025-12-09 14:36:00 +0100

    adding nerf_fourier to the param summary

--- src/model/mixer_model.py
+++ src/model/mixer_model.py
@@ -173,6 +173,8 @@ class MixerModel(nn.Module):
             summ += f"        Fourier Input Encoder: True\n Number of Frequency Bands: {self.num_freq_bands}\n Sigma: {self.pos_enc_sigma}\n"
         elif self.input_encoder == 'linear':
             summ += f"        Linear Input Encoder: True\n"
+        elif self.input_encoder == 'nerf_fourier':
+            summ += f"        NeRF Fourier Input Encoder: True\n"
         else:
             raise ValueError(f"Unsupported input_encoder: {self.input_encoder}")
         return summ
