fixing attention mask in feature enhancer

src/model/blocks.py
@@ -391,14 +391,14 @@ class FeatureEnhancer(nn.Module):
             x1 = x1 + self.__self_attention(self.first_attn_norm(x1),self.first_attn_dropout, self.first_self_attn, attn_mask=src1_mask, src_rope=src1_rope)
             x2 = x2 + self.__self_attention(self.second_attn_norm(x2),self.second_attn_dropout, self.second_self_attn, attn_mask=src2_mask, src_rope=src2_rope)
             x1 = x1 + self.__cross_attention(self.f2s_cross_attn_norm(x1), x2, self.f2s_cross_attn_dropout, self.f2s_cross_attn, attn_mask=src2_mask, src_rope=src1_rope, mem_rope=src2_rope)
-            x2 = x2 + self.__cross_attention(self.s2f_cross_attn_norm(x2), x1, self.s2f_cross_attn_dropout, self.s2f_cross_attn,attn_mask=src2_mask, src_rope=src2_rope, mem_rope=src1_rope)
+            x2 = x2 + self.__cross_attention(self.s2f_cross_attn_norm(x2), x1, self.s2f_cross_attn_dropout, self.s2f_cross_attn, attn_mask=src1_mask, src_rope=src2_rope, mem_rope=src1_rope)
             x1 = x1 + self.first_ff(self.first_ff_norm(x1))
             x2 = x2 + self.second_ff(self.second_ff_norm(x2))
         else:
             x1 = self.first_attn_norm(x1 + self.__self_attention(x1,self.first_attn_dropout, self.first_self_attn, attn_mask=src1_mask, src_rope=src1_rope))
             x2 = self.first_attn_norm(x2 + self.__self_attention(x2,self.second_attn_dropout, self.second_self_attn, attn_mask=src2_mask, src_rope=src2_rope))
             x1 = self.f2s_cross_attn_norm(x1 + self.__cross_attention(x1, x2, self.f2s_cross_attn_dropout, self.f2s_cross_attn, attn_mask=src2_mask, src_rope=src1_rope, mem_rope=src2_rope))
-            x2 = self.s2f_cross_attn_norm(x2 + self.__cross_attention(x2, x1, self.s2f_cross_attn_dropout, self.s2f_cross_attn,attn_mask=src2_mask, src_rope=src2_rope, mem_rope=src1_rope))
+            x2 = self.s2f_cross_attn_norm(x2 + self.__cross_attention(x2, x1, self.s2f_cross_attn_dropout, self.s2f_cross_attn,attn_mask=src1_mask, src_rope=src2_rope, mem_rope=src1_rope))
             x1 = self.first_ff_norm(x1 + self.first_ff(x1))
             x2 = self.second_ff_norm(x2 + self.second_ff(x2))
         