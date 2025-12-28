fixing wrong feature normalization

src/model/mixer_model.py
@@ -423,7 +423,7 @@ class MixerModel(nn.Module):
                     image_src = self.final_fsrc_norm_image(image_src)
                     img_enh = self.final_fenh_norm_image(img_enh)
             if self.use_enh_img_features and self.norm_first:
-                image_src = self.final_fenh_norm_image(image_src)
+                img_enh = self.final_fenh_norm_image(img_enh)
             if self.enh_features_dropout > 0:
                 img_enh = self.enh_features_dropout_nn(img_enh)
             if self.use_enh_img_features: