944b678025312fbccbe1247e6cbb89a669d769a6
lulloa <lulloa@datamart.co>
2025-12-29 18:37:44 +0100

    adding weight scheduler

---
+++ src/training/weights_scheduler.py
@@ -0,0 +1,23 @@
+import torch
+
+class WeightsScheduler:
+    def __init__(self,init_b, end_b, epochs, loss_function):
+        self.init_b = init_b
+        self.end_b = end_b
+        self.epochs = epochs
+        self.loss_function = loss_function
+        self.epoch = 0
+        self.x = torch.arange(15)
+        self.b = torch.linspace(0.8,0.999,epochs)
+    def compute_weights(self):
+        return (self.b[self.epoch]**self.x) + 1
+    
+    def start(self):
+        self.loss_function.set_weights(self.compute_weights())
+    
+    def update_epoch(self):
+        if self.epoch < self.epochs:
+            self.epoch += 1
+            self.loss_function.set_weights(self.compute_weights())
+        
+        
