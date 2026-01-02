4a345bfb67579442c7e6ffb174391c9862c1c44e
lulloa <lulloa@datamart.co>
2026-01-02 19:14:25 +0100

    fixing function typo

--- src/model/model_io.py
+++ src/model/model_io.py
@@ -98,7 +98,7 @@ def load_models_with_data(path_list):
         print(f"Loading pipeline from {path}")
         pipe = load_pipeline(path, pipe)
         pipe.load_dataset()
-        scheduled_sampling = pipe.load_scheduled_sampling()
+        scheduled_sampling = pipe.build_scheduled_sampling()
         if scheduled_sampling is not None:
             model.set_scheduled_sampling(scheduled_sampling)
         train, val, test = load_test_data(pipe, path)
