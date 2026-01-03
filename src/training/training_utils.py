c6469bc184adae29b0f3ff5821640a9a37218a38
lulloa <lulloa@datamart.co>
2026-01-03 15:06:26 +0100

    evaluating only in model outputs

--- src/training/training_utils.py
+++ src/training/training_utils.py
@@ -314,7 +314,7 @@ class ScheduledSampling:
                 break
             reg = concat_reg(output)
             current_step_pred = reg[:, -1:, :] 
-            if (torch.rand(1).item() < use_model_prob):
+            if (torch.rand(1).item() < use_model_prob) or not self.model.training:
                 next_token = current_step_pred.detach()
             else:
                 next_token = ori_tgt[:, t, :].unsqueeze(1)
