ba74cbf06f48d04e354abce9d9a7c3337a742ebb
lulloa <lulloa@datamart.co>
2026-01-04 14:42:12 +0100

    testing  on test set

--- src/notebooks/eval_batch.py
+++ src/notebooks/eval_batch.py
@@ -114,7 +114,7 @@ inputs_outputs = []
 
 models_and_data = load_models_with_data(ckpt_path)
 print(f'Model {names[0]}')
-for i, ((model, test_dataloader, _, _), ckpt_path, name) in enumerate(zip(models_and_data, ckpt_path, names)):    
+for i, ((model, _, _, test_dataloader), ckpt_path, name) in enumerate(zip(models_and_data, ckpt_path, names)):    
     model.eval()
     if model.scheduled_sampling is not None:
         model.scheduled_sampling.use_kv_cache = False
